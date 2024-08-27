bb37ab58a4560f7a3a5db43d65ef4d1d7afcdcad
Ahmed Shawky <Ahmed.M.Shawky98@gmail.com>
2024-08-27 18:25:09 +0300

    Update the Transceiver with CFO Effect & CFO Estimation and Correction Blocks Example File

--- Examples/Transceiver_CFO_example.m
+++ Examples/Transceiver_CFO_example.m
@@ -2,44 +2,34 @@ clc;clear;close all;
 tic;
 
 %% Wifi Packet Paramters
-LENGTH = 100;      % 1-4095
-DataRate = [6,9,12,18,24,36,48,54];      % 6,9,12,18,24,36,48,54  --6,9,36,54 errors
-ModOrder = [2,2,4,4,16,16,64,64];
+LENGTH = 1000;      % 1-4095                % Transmitter Default Length: 100
+DataRate = 36;      % 6,9,12,18,24,36,48,54 % Transmitter Default DataRate: 36
 
-%% Simulation paramters
-MaxSNR = 10;
-SNR = -10:1:MaxSNR;
-SNR_linear = 10.^(SNR/10);
-Iterations = 5;
+%% Creating Objects
+% Creat Transmiter Object
+Transmitter = IEEE802_11a_Transmitter(LENGTH,DataRate);
+Transmitter.DebugMode = 1; % Enable Transmitter & Receiver Debug Mode
 
-   
+% Creat Effects Object
+Effects = IEEE802_11a_Effects();
+Effects.DebugMode = 1; % Enable Effects Debug Mode
+    
+% Creat Receiver Object
+Receiver = IEEE802_11a_Receiver();
+Receiver.CFO_Mode = 1;  % Enable Carrier Offset Estimation & Compensation Block
+
 %% Data Generating
 data_hex = randi(255,LENGTH,1);
 data_bits = dec2bin(data_hex)-'0';
-
-%% Waveform Generating
-% Creat Transmiter Object
-Transmitter = IEEE802_11a_Transmitter(LENGTH);
-Transmitter.DebugMode = 1;
-
-% Generate Waveform
+%% Data Transmittion
 TX_Output = Transmitter.GenerateWaveform(data_hex);
+%% Adding Effects
+Effects.TransmitterOutput = TX_Output;
+Effects.add_CFO(100) %% Add 100% of Carrier offset
 
-%% Adding Multipath Channel effect
-Effects = IEEE802_11a_Effects(TX_Output);
-Effects.DebugMode = 1; % Enable Effects Debug Mode
-
-% Effects.add_Noise(15); %% SNR = 15
-Effects.add_CFO(20) %% Ratio of added Carrier offset
-
-
-%% Extracting Data
-% Creat Receiver Object
-Receiver = IEEE802_11a_Receiver(Effects.TransmitterOutput);%IEEE802_11a_Receiver(TX_Output);
-Receiver.EqualizerMode = 1;
-Receiver.CFO_Mode = 1;
+%% Receiving Data
 try
-    RX_Data = Receiver.ReceiveData();
+    RX_Data = Receiver.ReceiveData(Effects.TransmitterOutput);
 catch Error
     RX_Data = 0;
     %disp('Error Message:')
@@ -54,11 +44,12 @@ catch Error
         errorLine = Error.stack.line;disp(errorLine)
     end
 end
-
+%% Calculate Bit Error Rate (BER)
 RX_data_bits = dec2bin(RX_Data)-'0';
 ByteError = sum(RX_Data ~= data_hex)/LENGTH;
 BitError = sum(sum(RX_data_bits ~= data_bits))/(LENGTH*8);
 
-
 disp("Byte Error: ");disp(ByteError);
 disp("Bit Error: ");disp(BitError);
+
+toc;
